49803f84f79a62f8ea2e7124c4f12100af04fc96
HasanMasud1039 <hasanmasud1039@gmail.com>
2023-03-16 11:14:14 +0600

    first

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
@@ -64,7 +64,7 @@ const displayQuiz = (data) => {
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
@@ -74,10 +74,20 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+const submit = document.querySelector("#submit");
+submit.addEventListener("click", () => {
+  console.log('clickeddddddddddd  '+ answers.length);
   if (answers.length < 6) {
+
+    // submit.setAttribute('disabled',"");
     return;
   }
+  else{
+    console.log('clicked');
+    // submit.removeAttribute('disabled',"");
+    
+  }
+
   quizTimer(true);
   answersContainer.innerHTML = `<div class="my-4">
   <i class="fa-solid fa-fan animate-spin text-2xl text-green-600"></i>
